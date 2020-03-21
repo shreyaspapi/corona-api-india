221afbb0514e5f2ed06f16f5471505996fa17497
Shreyas Papinwar <spapinwar@gmail.com>
2020-03-21 17:05:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,25 +3,27 @@ import requests
 from flask import jsonify
 import json
 
-data = requests.get("https://api.rootnet.in/covid19-in/stats/latest")
-
-di = data.json()
 
 app = Flask(__name__)
 
 
 @app.route("/data")
 def state_data():
-    data_list = di["data"]["regional"]
-    dd = di["data"]["summary"]
-    
-    data_list.insert(0, {"loc": "Total", "total": dd["total"], "confirmedCasesIndian": dd["confirmedCasesIndian"], "confirmedCasesForeign": dd["confirmedCasesForeign"], "discharged": dd["discharged"], "deaths": dd["deaths"]})
+    data = requests.get("https://api.rootnet.in/covid19-in/stats/latest")
 
-    for data in range(len(data_list)):
+    di = data.json()
+
+    dd = di["data"]["summary"]
+    data_list = di["data"]["regional"]
+
+    data_list.insert(0, {"id": 0,"loc": "Total", "total": str(dd["total"]), "confirmedCasesIndian": str(dd["confirmedCasesIndian"]), "confirmedCasesForeign": str(dd["confirmedCasesForeign"]), "discharged": str(dd["discharged"]), "deaths": str(dd["deaths"])})
+
+
+    for data in range(1, len(data_list)):
         a = data_list[data]["confirmedCasesIndian"]
         b = data_list[data]["confirmedCasesForeign"]
         data_list[data]["total"] = str(a + b)
-        data_list[data]["id"] = data
+        data_list[data]["id"] = data + 1
         data_list[data]["confirmedCasesIndian"] = str(data_list[data]["confirmedCasesIndian"])
         data_list[data]["confirmedCasesForeign"] = str(data_list[data]["confirmedCasesForeign"])
         data_list[data]["discharged"] = str(data_list[data]["discharged"])
