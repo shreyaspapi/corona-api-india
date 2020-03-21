24df2e47ceeb75612a298b2e6f6544d158b0dd72
Shreyas Papinwar <spapinwar@gmail.com>
2020-03-21 16:41:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,10 @@ app = Flask(__name__)
 @app.route("/data")
 def state_data():
     data_list = di["data"]["regional"]
+    dd = di["data"]["summary"]
     
+    data_list.insert(0, {"loc": "Total", "total": dd["total"], "confirmedCasesIndian": dd["confirmedCasesIndian"], "confirmedCasesForeign": dd["confirmedCasesForeign"], "discharged": dd["discharged"], "deaths": dd["deaths"]})
+
     for data in range(len(data_list)):
         a = data_list[data]["confirmedCasesIndian"]
         b = data_list[data]["confirmedCasesForeign"]
@@ -25,10 +28,6 @@ def state_data():
         data_list[data]["deaths"] = str(data_list[data]["deaths"])
     return json.dumps(data_list)
 
-@app.route("/data_total")
-def data_total():
-    data_list = di["data"]["summary"]
-    return jsonify(data_list)
 
 if __name__ == '__main__':
    app.run()
