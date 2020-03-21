ee5d9f17d8e7a5697630adc343de29a5b5f2da03
Shreyas Papinwar <spapinwar@gmail.com>
2020-03-21 16:09:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,8 +17,12 @@ def state_data():
     for data in range(len(data_list)):
         a = data_list[data]["confirmedCasesIndian"]
         b = data_list[data]["confirmedCasesForeign"]
-        data_list[data]["total"] = a + b 
+        data_list[data]["total"] = str(a + b)
         data_list[data]["id"] = data
+        data_list[data]["confirmedCasesIndian"] = str(data_list[data]["confirmedCasesIndian"])
+        data_list[data]["confirmedCasesForeign"] = str(data_list[data]["confirmedCasesForeign"])
+        data_list[data]["discharged"] = str(data_list[data]["discharged"])
+        data_list[data]["deaths"] = str(data_list[data]["deaths"])
 
 
 
