94c79b1e7a9f6739c11978e3e12b1e407e47f96d
Shreyas Papinwar <spapinwar@gmail.com>
2020-03-21 16:16:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,11 +23,12 @@ def state_data():
         data_list[data]["confirmedCasesForeign"] = str(data_list[data]["confirmedCasesForeign"])
         data_list[data]["discharged"] = str(data_list[data]["discharged"])
         data_list[data]["deaths"] = str(data_list[data]["deaths"])
-
-
-
     return json.dumps(data_list)
 
+@app.route("/data_total")
+def data_total():
+    data_list = di["data"]["summary"]
+    return jsonify(data_list)
 
 if __name__ == '__main__':
    app.run()
