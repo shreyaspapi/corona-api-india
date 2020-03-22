647a6363fd62027b4798c4fffc4289983487cf50
Shreyas Papinwar <spapinwar@gmail.com>
2020-03-22 12:57:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,27 +7,28 @@ import json
 app = Flask(__name__)
 
 
+
 @app.route("/data")
 def state_data():
-    data = requests.get("https://api.rootnet.in/covid19-in/stats/latest")
+    data = requests.get("https://api.covid19india.org/data.json")
+    data = data.json()["statewise"]
+    d = []
+    for i in data:
+        d.append(dict([(str(k), str(v)) for k, v in i.items()]))
 
-    di = data.json()
+    data_list = []
 
-    dd = di["data"]["summary"]
-    data_list = di["data"]["regional"]
-
-    data_list.insert(0, {"id": 0,"loc": "Total", "total": str(dd["total"]), "confirmedCasesIndian": str(dd["confirmedCasesIndian"]), "confirmedCasesForeign": str(dd["confirmedCasesForeign"]), "discharged": str(dd["discharged"]), "deaths": str(dd["deaths"])})
-
-
-    for data in range(1, len(data_list)):
-        a = data_list[data]["confirmedCasesIndian"]
-        b = data_list[data]["confirmedCasesForeign"]
-        data_list[data]["total"] = str(a + b)
-        data_list[data]["id"] = data + 1
-        data_list[data]["confirmedCasesIndian"] = str(data_list[data]["confirmedCasesIndian"])
-        data_list[data]["confirmedCasesForeign"] = str(data_list[data]["confirmedCasesForeign"])
-        data_list[data]["discharged"] = str(data_list[data]["discharged"])
-        data_list[data]["deaths"] = str(data_list[data]["deaths"])
+    for k in range(len(d)):
+        data_dict = {}
+        if d[k]["confirmed"] == "0":
+            break
+        data_dict["total"] = d[k]["confirmed"]
+        data_dict["id"] = k
+        data_dict["confirmedCasesIndian"] = d[k]["active"]
+        data_dict["confirmedCasesForeign"] = d[k]["active"]
+        data_dict["discharged"] = d[k]["recovered"]
+        data_dict["deaths"] = d[k]["deaths"]
+        data_list.append(data_dict)
     return json.dumps(data_list)
 
 
